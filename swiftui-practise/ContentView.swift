1abaf29cbf99b8e86e38d7b0fd895e4f866fe618
SaujanBindukar <34705432+SaujanBindukar@users.noreply.github.com>
2024-04-23 22:27:07 +0545

    Initial Commit

---
+++ swiftui-practise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-practise
+//
+//  Created by Saujan Bindukar on 23/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-practise/swiftui_practiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_practiseApp.swift
+//  swiftui-practise
+//
+//  Created by Saujan Bindukar on 23/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_practiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
