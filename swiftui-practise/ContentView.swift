00baf30c466c4552458743710c64d605af14c9f2
SaujanBindukar <34705432+SaujanBindukar@users.noreply.github.com>
2024-04-23 22:40:42 +0545

    used modifier for text

--- swiftui-practise/ContentView.swift
+++ swiftui-practise/ContentView.swift
@@ -9,14 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+            
             Text("Hello, world!")
+                .font(.body)
+                .padding(20)
+                .background(Color.red)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .fontWeight(.black)
+            
+            Text("Hello, world!")
+                .padding(EdgeInsets(top: 20, leading: 10, bottom: 10, trailing: 10))
+            
+            Text("Hello, world! again")
+            Spacer()
         }
-        .padding()
+        
     }
+    
 }
 
 #Preview {
