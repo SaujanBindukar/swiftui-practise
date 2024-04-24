515671953dec3913632a2d52362922431f20b9ad
SaujanBindukar <34705432+SaujanBindukar@users.noreply.github.com>
2024-04-24 22:37:20 +0545

    Basic view created using HStack, VStack, ZStack, Image, Text

--- swiftui-practise/MoviePageView.swift
+++ swiftui-practise/MoviePageView.swift
@@ -9,10 +9,37 @@ import SwiftUI
 
 struct MoviePageView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
+            HStack{
+                
+                Image(systemName: "photo").resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .foregroundColor(Color.orange)
+                    .padding(.all)
+                    .frame(width: 100.0, height: 100.0)
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/).shadow(color: .gray, radius: 10.0, x: 4.0, y: 4.0)
+                
+                VStack(alignment:.leading,spacing: 4.0){
+                    Text("Saujan Bindukar")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                    Text("Kathmandu, Nepal")
+                        .font(.subheadline)
+                    Group{
+                        Text("Street name is Nothing")
+                            
+                        Text("This is the last text for this view")
+                            
+                    }.font(.caption).foregroundColor(.red).font(.headline).fontWeight(.bold)
+                }
+                Spacer()
+                
+            }
+        Spacer()
     }
 }
 
 #Preview {
-    MoviePageView()
+    MoviePageView().padding(20)
+        .previewLayout(.sizeThatFits)
 }
--- swiftui-practise/swiftui_practiseApp.swift
+++ swiftui-practise/swiftui_practiseApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct swiftui_practiseApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MoviePageView()
         }
     }
 }
