26c593655ef82a7b4cc6f917efbc269441d64d2e
SaujanBindukar <34705432+SaujanBindukar@users.noreply.github.com>
2024-04-24 22:35:41 +0545

    Basic view created using HStack, VStack, Text, Image

---
+++ swiftui-practise/MoviePageView.swift
@@ -0,0 +1,18 @@
+//
+//  MoviePageView.swift
+//  swiftui-practise
+//
+//  Created by Saujan Bindukar on 24/04/2024.
+//
+
+import SwiftUI
+
+struct MoviePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoviePageView()
+}
